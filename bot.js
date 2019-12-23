373c72fd40ab39308566435741b5ca61bbb58611
Hosuk Choi <hosukisawesome@hotmail.com>
2019-12-23 14:58:55 -0500

    new bot stuff

--- bot.js
+++ bot.js
@@ -3,7 +3,13 @@ const client = new Discord.Client(); //so we need to import these
 const auth = require('./auth.json');//importing
 
 client.on('ready', () => {
-    console.log('Logged in as ${client.user.tag}!');
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
 });
 
 client.login(auth.token);
